e7f38b1714203ba376f5d783742fb7b5467c622e
Christopher Pappas <damassi.pappas@gmail.com>
2015-06-26 18:35:17 -0700

    Initial file addition

---
+++ common/models/reviewer.js
@@ -0,0 +1,13 @@
+var Promise = require('bluebird');
+
+module.exports = function(Reviewer) {
+
+  // Reviewer.on('dataSourceAttached', function (obj) {
+  //   Reviewer = Promise.promisifyAll(Reviewer, {
+  //     filter: function (name, func, target) {
+  //       return !(name ==='validate');
+  //     }
+  //   });
+  // });
+
+};
