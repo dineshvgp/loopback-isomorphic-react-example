539cf818075b825b6bb1abda59155635a11f4033
Christopher Pappas <damassi.pappas@gmail.com>
2015-06-26 18:48:26 -0700

    Added react files

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,10 @@
 module.exports = function(Review) {
 
+  Review.beforeRemote('create', function(context, user, next) {
+    const req = context.req;
+    req.body.date = Date.now();
+    req.body.publisherId = req.accessToken.userId;
+    next();
+  });
+
 };
